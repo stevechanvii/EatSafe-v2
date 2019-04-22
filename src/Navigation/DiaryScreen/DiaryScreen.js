75ada85068d1db6d5cb301e1218bb6852c8be5b4
stevechanvii <dche0007@student.monash.edu>
2019-04-22 20:17:47 +1000

    diary screen

--- src/Navigation/DiaryScreen/DiaryScreen.js
+++ src/Navigation/DiaryScreen/DiaryScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Text, Header, Thumbnail, Content, H1 } from 'native-base';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Container, Text, Thumbnail, Content } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
+import Icon from 'react-native-vector-icons/Entypo';
 import uri from '../../asserts/logo.jpg';
 
 
@@ -10,20 +11,59 @@ export default class diaryScreen extends Component {
     title: 'Home',
   };
 
+  state = {
+    date: new Date(),
+  }
+
+  getDay = date => {
+    // const date = new Date();
+    const days = {
+      0: 'Sun',
+      1: 'Mon',
+      2: 'Tue',
+      3: 'Wed',
+      4: 'Thu',
+      5: 'Fri',
+      6: 'Sat'
+    }
+    // alert(days[date.getDay()]);
+    return days[date.getDay()];
+  }
+
+  setDayHandler = operator => {
+    const nextDay = this.state.date;
+    nextDay.setDate(nextDay.getDate() + operator);
+    this.setState({ date: nextDay });
+  }
+
   render() {
     return (
       <Container>
-        {/* <Thumbnail large source={uri} />
-        <Text style={{ color: "black", fontSize: 24, fontWeight: "bold" }}>Diary</Text>
-        <Text style={{ color: "black", fontSize: 18 }}>Coming Soon</Text> */}
         <Grid>
           <Row size={3}>
-            <Col size={2} style={{backgroundColor: '#00CE9F'}}><Text>L</Text></Col>
-            <Col size={5} style={styles.datePicker}><Thumbnail large source={uri} /></Col>
-            <Col size={2} style={{backgroundColor: '#00CE9F'}}><Text>R</Text></Col>
+            <Col size={2} style={styles.arrowBtn}>
+              <TouchableOpacity onPress={() => this.setDayHandler(-1)}>
+                <Icon name='chevron-thin-left' size={50} />
+              </TouchableOpacity>
+            </Col>
+            <Col size={5} style={styles.datePicker}>
+              <Thumbnail large source={uri} />
+              <Text>{this.getDay(this.state.date)}</Text>
+            </Col>
+            <Col size={2} style={styles.arrowBtn}>
+              <TouchableOpacity onPress={() => this.setDayHandler(1)}>
+                <Icon name='chevron-thin-right' size={50} />
+              </TouchableOpacity>
+            </Col>
           </Row>
-          <Row size={7} style={{backgroundColor: '#635DB7'}}>
-            <Col style={{backgroundColor: '#00CE9F', height: 200}}></Col>
+          <Row size={7} style={{ backgroundColor: '#635DB7' }}>
+            <Content>
+              <Col style={styles.records}></Col>
+              <Col style={styles.records}></Col>
+              <Col style={styles.records}></Col>
+              <Col style={styles.records}></Col>
+              <Col style={styles.records}></Col>
+            </Content>
           </Row>
         </Grid>
       </Container>
@@ -53,5 +93,16 @@ const styles = StyleSheet.create({
   datePicker: {
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  records: {
+    backgroundColor: '#00CE9F',
+    height: 80,
+    marginHorizontal: '3%',
+    marginTop: '3%'
+  },
+  arrowBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00CE9F'
   }
 });
