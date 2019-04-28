455d66e672327894a87fddbe7efd0a2ca0826d9d
stevechanvii <dche0007@student.monash.edu>
2019-04-28 20:28:02 +1000

    remove Symptoms hardcode

--- src/Preferences/Preferences.js
+++ src/Preferences/Preferences.js
@@ -1,4 +1,10 @@
-const Meals = ['Breakfast', 'Brunch', 'Lunch', 'Afternoon Tea', 'Dinner', 'Midnight Sanck'];
+const Meals = [
+    'Breakfast', 
+    'Brunch', 
+    'Lunch', 
+    'Afternoon Tea', 
+    'Dinner', 
+    'Midnight Sanck'];
 
 const Feelings = {
     'Excellent': 'emoticon-cool-outline',
@@ -8,4 +14,21 @@ const Feelings = {
     'Awful': 'emoticon-poop'
 };
 
-export default { Meals, Feelings };
+const Allergens = {
+
+};
+
+const Symptoms = [
+    'Diarrhea',
+    'Bloating',
+    'Rashes',
+    'Headaches',
+    'Nausea',
+    'Fatigue',
+    'Abdominal Pain',
+    'Runny Nose',
+    'Reflux',
+    'Flushing of Skin'
+];
+
+export default { Meals, Feelings, Symptoms };
