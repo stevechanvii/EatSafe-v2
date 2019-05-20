399e99dc8b853dc119cff8983481507d138b14a0
stevechanvii <dche0007@student.monash.edu>
2019-05-21 00:37:28 +1000

    Implement allergen detector in text recognition

--- src/Navigation/ScanningScreen/RecognitionResultScreen.js
+++ src/Navigation/ScanningScreen/RecognitionResultScreen.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Image, AsyncStorage, StyleSheet } from 'react-native';
-import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Left, Body, H2, Accordion } from 'native-base';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Container, Content, H3, View, Thumbnail, Text, Button, Left, Body, H2, Accordion } from 'native-base';
 import defaultIngredients from '../../Preferences/Ingredients.js';
+import Modal from "react-native-modal";
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import HeaderGoBack from '../../Components/HeaderGoBack';
+import ScanSVG from '../../assets/svg/scan_svg';
+import Preference from '../../Preferences/Preferences';
+import AllergensDetector from '../../Utils/AllergensDetector';
 
 var stringSimilarity = require('string-similarity');
 
 const excluterms = ["nutrition", "information", "serving", "per", "package", "size", "energy", "protein", "total", "carbohydrate", "sodium", "average", "quantity", "daily", "intake", "ml", "mg", "g", "kj", "contains", "caffeine", "calcium"]
 
+/**
+ * @class recognitionResultScreen shows the result after user use text recognition,
+ * it also give alert when user set the allergens or intolerance
+ * 
+ * this component also perform data clearning and validation
+ * 
+ * this component is in mess, clearning should move to utils
+ */
 export default class recognitionResultScreen extends Component {
     static navigationOptions = {
         header: null
@@ -16,15 +29,114 @@ export default class recognitionResultScreen extends Component {
 
     state = {
         isLoading: false,
-        textList: []
+        // textList: [],
+
+        ingredList: [],
+        isModalVisible: false,
+        alert: [],
     }
 
-    componentDidMount() {
-        const { navigation } = this.props;
+    /**
+     * @func toggleModal haldle whether show allergen detected alert
+     */
+    toggleModal = () => {
+        this.setState({ isModalVisible: !this.state.isModalVisible });
+    };
 
-        this.setState({
-            textList: navigation.getParam('textList', 'No-text')
-        })
+    /**
+     * @func componentWillMount call userAllergensAlert() when component initialized
+     */
+    componentWillMount = () => {
+        const ingredList = this.dataClearning();
+        this.userAllergensAlert(ingredList);
+    }
+
+    /**
+     * @func userAllergensAlert retrive the allergens and intolerance from Async Storage and compare with
+     * preset allergen and intolerance map in Preference, this function is performed by AllergensDetector.js
+     */
+    userAllergensAlert = async (ingredList) => {
+        // check ingredientList exist first!
+        console.log(ingredList)
+        if (ingredList.length === 0) {
+            return;
+        }
+
+        try {
+            // get all the allergens and intolerance from database
+            let values;
+            try {
+                values = await AsyncStorage.multiGet(['allergens', 'intolerance']);
+            } catch (e) {
+                // read error
+                console.log(e);
+            }
+
+            const allergenObj = {};
+            // iterate all allergens and intolerance and find allergens only true
+            values.map(el => {
+                if (el[1]) {
+                    Object.entries(JSON.parse(el[1])).forEach(([key, value]) => {
+                        // find corresponding value in Preference
+                        if (JSON.parse(value)) {
+                            if (key in Preference.Allergens) {
+                                allergenObj[key] = Preference.Allergens[key];
+                            } else if (key in Preference.Intolerance) {
+                                allergenObj[key] = Preference.Intolerance[key];
+                            } else {
+                                allergenObj[key] = [];
+                            }
+                        }
+                    });
+                }
+            });
+
+            console.log(allergenObj)
+            // return if no sensitive ingredients found
+            if (Object.entries(allergenObj).length === 0 && allergenObj.constructor === Object) {
+                return
+            }
+
+            // compare the allergens and intolerence which user suffer from with the ingredients
+            const diagnose = AllergensDetector(allergenObj, ingredList);
+            console.log(diagnose)
+            if (diagnose.length > 0) {
+                // remove duplicant in diagnose
+                diagnoseCleaned = [...new Set(diagnose)];
+
+                this.setState({ alert: diagnoseCleaned });
+                this.toggleModal();
+            }
+
+        } catch (e) {
+            console.log(e);
+            // read error
+        }
+    }
+    /**
+         * @func renderModalContent modal content (alert)
+         */
+    renderModalContent = () => (
+        <View style={styles.contentModal}>
+            <Text style={styles.contentTitle}>Allergens Detected!</Text>
+            <Text style={{ padding: 10 }}>{this.state.alert.join(', ')}</Text>
+            <TouchableOpacity onPress={this.toggleModal} style={styles.allergenAllertBtn}>
+                <Text style={styles.allergenAllertText}>Close</Text>
+            </TouchableOpacity>
+            {/* <Button
+                onPress={this.toggleModal}
+                style={styles.allergenBtn}>
+                <Text>Close</Text>
+            </Button> */}
+        </View>
+    );
+
+    componentDidMount() {
+        // const { navigation } = this.props;
+
+        // this.setState({
+        //     textList: navigation.getParam('textList', 'No-text')
+        // })
     }
 
     prepText(text) {
@@ -81,7 +193,7 @@ export default class recognitionResultScreen extends Component {
         return ingPresent
     }
 
-    render() {
+    dataClearning = () => {
         textListAdv = []
         clearText = []
         uniqtext = []
@@ -90,26 +202,41 @@ export default class recognitionResultScreen extends Component {
         finaltext = []
         ingredList = []
         Ingredients = defaultIngredients
-        this.state.textList.map(el => (textListAdv = textListAdv.concat(el.toLowerCase().split("\n"))))
+        this.props.navigation.getParam('textList', 'No-text').map(el => (textListAdv = textListAdv.concat(el.toLowerCase().split("\n"))))
         textListAdv.map(el => (clearText = clearText.concat(this.prepText(el))))
         clearText.map(el => ((!uniqtext.includes(el)) & el.length > 2 ? uniqtext = uniqtext.concat(el) : null))
         uniqtext.map(el => (!this.subtextCheck(el, uniqtext) ? subText = subText.concat(el) : null))
         text_ingred = subText.join(" ")
         Ingredients.map(el => (text_ingred.includes(el) ? ingredList = ingredList.concat(el) : null))
 
+        console.log(ingredList);
+        this.setState({ ingredList: ingredList });
+        console.log(this.state.ingredList)
+        return ingredList;
+    }
+
+    render() {
         return (
             <Container>
-                <HeaderGoBack navigation={this.props.navigation} title='Text Recognition' />
+                <HeaderGoBack navigation={this.props.navigation} title='Recognition' />
                 <Content>
                     <Grid>
                         <Row style={styles.gridCenter}>
-                            <Image source={require('../../assets/img/icons8-rescan-document-100.png')} />
+                            <ScanSVG.TextScan height={200} width={200} />
                         </Row>
                         <Row style={styles.gridCenter}>
-                            <Text>We found following Ingredients</Text>
+                            {console.log(this.state)}
+                            <H3 style={{ marginTop: 40 }}>{this.state.ingredList.length > 0 ? 'We found following ingredients' : 'No ingredients found'}</H3>
                         </Row>
-                        <Row>
-                            <Text>{ingredList.join(', ')}</Text>
+                        <Row style={styles.gridCenter}>
+                            <Text style={{ margin: 30 }}>{this.state.ingredList.join(', ')}</Text>
+                            <Modal
+                                isVisible={this.state.isModalVisible}
+                                animationIn="slideInUp"
+                                animationOut="slideOutDown"
+                            >
+                                {this.renderModalContent()}
+                            </Modal>
                         </Row>
                         <Button
                             style={styles.addDiaryBtn}
@@ -118,14 +245,12 @@ export default class recognitionResultScreen extends Component {
                                     'CreateDairy',
                                     {
                                         navigation: this.props.navigation,
-                                        ingredients: ingredList
+                                        ingredients: this.state.ingredList
                                     });
                             }} >
                             <Text>Add to Dairy</Text>
                         </Button>
                     </Grid>
-
-
                 </Content>
             </Container>
         );
@@ -174,13 +299,31 @@ const styles = StyleSheet.create({
     },
     contentTitle: {
         fontSize: 20,
-        marginBottom: 12,
+        marginTop: 15
     },
-    allergenBtn: {
+    allergenAllertBtn: {
+        marginTop: 7,
         backgroundColor: '#FB9D5D',
-        alignSelf: 'center',
+        width: '100%',
+        height: 45,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    allergenAllertText: {
+        alignSelf: 'center',
+        color: 'white',
+        fontSize: 18
+    },
+    contentModal: {
+        backgroundColor: 'white',
+        // padding: 22,
+        margin: 27,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
     },
     gridCenter: {
         justifyContent: 'center',
--- src/Navigation/ScanningScreen/Result/LostNetwork.js
+++ src/Navigation/ScanningScreen/Result/LostNetwork.js
@@ -3,11 +3,14 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Container, Header, Content, Thumbnail, Text, Button, Badge, Body, ListItem } from 'native-base';
 
-const productNotFound = () => (
+/**
+ * @class lostNetwork is the child component of productDetailScreen.js
+ */
+const lostNetwork = () => (
     <Container style={{ alignItems: 'center', justifyContent: 'center' }}>
         <Icon name='comment-alert-outline' size={40} />
         <Text style={{ color: "black", fontSize: 20, fontWeight: "bold" }}>Sorry, Lost Network Connection!</Text>
     </Container>
 );
 
-export default productNotFound;
+export default lostNetwork;
